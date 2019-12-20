39c9971a3464e5439b4a2c1ac22d60d51136d503
Arkanic <50847107+Arkanic@users.noreply.github.com>
2019-12-20 22:38:25 +1300

    Create scatter-silo.js

---
+++ scripts/scatter-silo.js
@@ -0,0 +1,33 @@
+//create a simple shockwave effect
+const siloLaunchEffect = newEffect(20, e => {
+    Draw.color(Color.white, Color.lightGray, e.fin()); //color goes from white to light gray
+    Lines.stroke(e.fout() * 3); //line thickness goes from 3 to 0
+    Lines.circle(e.x, e.y, e.fin() * 100); //draw a circle whose radius goes from 0 to 100
+});
+
+//create the block type
+const silo = extendContent(Block, "scatter-silo", {
+    //override the method to build configuration
+    buildConfiguration(tile, table){
+        table.addImageButton(Icon.arrowUpSmall, Styles.clearTransi, run(() => {
+            //configure the tile to signal that it has been pressed (this sync on client to server)
+            tile.configure(0)
+        })).size(50)
+    },
+
+    //override configure event
+    configured(tile, value){
+        //make sure this silo has the items it needs to fire
+        if(tile.entity.cons.valid()){
+            //make this effect occur at the tile location
+            Effects.effect(siloLaunchEffect, tile)
+
+            //create 10 bullets at this tile's location with random rotation and velocity/lifetime
+            for(var i = 0; i < 10; i++){
+                Calls.createBullet(Bullets.flakExplosive, tile.getTeam(), tile.drawx(), tile.drawy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
+            }
+            //triggering consumption makes it use up the items it requires
+            tile.entity.cons.trigger()
+        }
+    }
+})
